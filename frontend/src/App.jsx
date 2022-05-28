c48e8d20c640fa5046527a04882e9fb1c9b604db
Fibre44 <denis.nicolas@hotmail.fr>
2022-05-28 11:05:43 +0200

    feat : le composant App.jsx va essayer de se connecter à l'application si on a un cookie on pas besoin de saisir les informations et si on actualise on reste bien connecté

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import './style/body.sass';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Login } from './components/users/Login'
 import { Site } from './components/Site'
 import { getData } from './api/api';
 
 export default function App() {
   const [credentials, setCredentials] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getData('/users/identity')
+        const userIdentity = await response.json()
+        setCredentials(() => true)
+      } catch {
+        setCredentials(() => false)
+      }
+    }
+    fetchData();
+  }, []);
 
   //Le cookie est sur httpOnly donc on ne peut pas le récupérer 
   //Solution on va envoyer une requete de test si le status est Ok ca signifie qu'on a bien un cookie sinon on affiche la page de connexion
