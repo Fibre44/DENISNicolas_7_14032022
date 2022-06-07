3ecb10d885a400c37c21a238bebbab3e62aa24bc
Fibre44 <denis.nicolas@hotmail.fr>
2022-06-07 21:34:40 +0200

    fix : Suppression de react paginate

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, memo } from "react";
-import ReactPaginate from 'react-paginate';
 import './../../style/message.sass';
 import { EditFormMessage } from "./EditFormMessage";
 import { FormComment } from "../comments/FormComment";
@@ -8,9 +7,6 @@ import { getData, setData } from './../../api/api'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faTrash, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import logo from './../../img/icon-left-font-monochrome-black.png'
-
-
-const items = [];
 
 export function Messages({ messages, refreshMessage, actifGroup, identity, messagesLikes, likesUser, pictures }) {
     if (messages) {
@@ -150,59 +146,4 @@ function MessageIcons({ setEditMode, messageId, actifGroup, refreshMessage }) {
     </div>
 
 }
-
-function Items({ currentItems }) {
-    return (
-        <>
-            {currentItems &&
-                currentItems.map((item) => (
-                    <div>
-                        <h3>Item #{item}</h3>
-                    </div>
-                ))}
-        </>
-    );
-}
-
-function PaginatedItems({ itemsPerPage }) {
-    // We start with an empty list of items.
-    const [currentItems, setCurrentItems] = useState(null);
-    const [pageCount, setPageCount] = useState(0);
-    // Here we use item offsets; we could also use page offsets
-    // following the API or data you're working with.
-    const [itemOffset, setItemOffset] = useState(0);
-
-    useEffect(() => {
-        // Fetch items from another resources.
-        const endOffset = itemOffset + itemsPerPage;
-        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-        setCurrentItems(items.slice(itemOffset, endOffset));
-        setPageCount(Math.ceil(items.length / itemsPerPage));
-    }, [itemOffset, itemsPerPage]);
-
-    // Invoke when user click to request another page.
-    const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % items.length;
-        console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
-        setItemOffset(newOffset);
-    };
-
-    return (
-        <>
-            <Items currentItems={currentItems} />
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="Page suivante >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={5}
-                pageCount={pageCount}
-                previousLabel="< Page précédente"
-                renderOnZeroPageCount={null}
-            />
-        </>
-    );
-}
-
 
